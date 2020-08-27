fe16255bde24eb65cc483004d732290c351e60b4
RutujaBiradar <rutujabiradar596@gmail.com>
2020-08-27 14:00:50 -0400

    just commit

---
+++ add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash _x
+read -p "enter first number" number1
+read -p "enter  second number" number2
+read -p "enter third number" number3
+read -p "enter forth number" number4
+echo $ "addition is"
+echo $ "substraction is"
+echo $ "mul is"
+echo $ " division is"
